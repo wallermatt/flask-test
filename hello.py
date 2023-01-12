7c2d6cb0b2e202bfc23316a1b061be3f166fcc9b
Matthew Waller <waller.matthew.alt@gmail.com>
2023-01-12 14:24:58 +0000

    post

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 # flask --app hello run
 # flask --app hello --debug run
 
-from flask import Flask
+from flask import Flask, url_for, request
 from markupsafe import escape
 
 
@@ -10,6 +10,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello World</p>"
+
+
+@app.route("/index/")
+def index():
+    return "<p>Index</p>"
 
 
 @app.route("/<path:name>")
@@ -21,4 +26,18 @@ def hello(name):
 def show_post(post_id):
     # show the post with the given id, the id is an integer
     return f'Post {post_id}'
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        return request.headers.get("key1")
+    else:
+        return "boring old get"
+
+
+with app.test_request_context():
+    print(url_for('hello_world'))
+    print(url_for('index'))
+    print(url_for("hello", name="test"))
 
